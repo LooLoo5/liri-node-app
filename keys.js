1ea8677052b31d5941dc4129e31f8b7fc47088f0
Emily Lorraine <emilylorraine@Emilys-MacBook-Air.local>
2018-10-17 18:28:19 -0500

    adding env

--- keys.js
+++ keys.js
@@ -1,6 +1,9 @@
 
 //Spotify API key
-module.exports = {
-    id: "7f77a221a5014883b651e5d1a8dfa99d",
-    secret: "2dc2e88dd1944f0d9c08005094ccff5f"
-}
+console.log('this is loaded');
+
+exports.spotify = {
+  id: process.env.SPOTIFY_ID,
+  secret: process.env.SPOTIFY_SECRET
+};
+
